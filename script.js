6f4f75437db7004e72923c6607a12ec056ac2d08
AndriySafonov <andreygold18@gmail.com>
2023-03-02 21:10:56 +0200

    create mob-menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuBtn = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+menuBtn.addEventListener("click", function () {
+  menuBtn.classList.toggle("active");
+  menu.classList.toggle("active");
+});
